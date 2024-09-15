ccec6e8d15a1e853f88b6c6de29a05cc3914225a
mlghammit <58157953+mlghammit@users.noreply.github.com>
2024-09-15 00:10:54 -0400

    Fixed home page words
    Need to fix second button, leads to page shouldn't exist

--- web/app/(splash)/GetStarted/GetStarted.tsx
+++ web/app/(splash)/GetStarted/GetStarted.tsx
@@ -27,109 +27,17 @@ export const GetStarted = () => {
             className="rounded-full"/>
         </h1>
         <div className="mb-8 text-center text-lg text-muted-foreground">
-          Build a realtime full-stack app in no time.
+          Get the best person for your, <strong>HACKATHON</strong>, <strong>JOB</strong> or any issue you might have with <strong>HACKD</strong>
         </div>
         <div className="mb-16 flex justify-center gap-4">
           <Button asChild size="lg">
-            <Link href="/hacker">Get Started</Link>
+            <Link href="/hacker">Hackers</Link>
           </Button>
           <Button asChild size="lg" variant="outline">
-            <Link href="https://docs.convex.dev/home">Convex docs</Link>
+            <Link href="https://docs.convex.dev/home">Recruiters</Link>
           </Button>
         </div>
-        {/* <div className="flex flex-col gap-4 bg-muted/50 p-12 dark:bg-transparent">
-          <h2 className="mb-1 text-center text-3xl font-bold md:text-4xl ">
-            Next steps
-          </h2>
-          <div className="mb-1 text-center text-muted-foreground">
-            This template is a starting point for building your web application.
-          </div>
-          <div className="grid gap-4 md:grid-cols-2">
-            <Card>
-              <CardHeader>
-                <CardTitle className="flex gap-2">
-                  <PlayIcon /> Play with the app
-                </CardTitle>
-              </CardHeader>
-              <CardContent>
-                Click on{" "}
-                <Link
-                  href="/product"
-                  className="font-medium underline underline-offset-4 hover:no-underline"
-                >
-                  Get Started
-                </Link>{" "}
-                to see the app in action.
-              </CardContent>
-            </Card>
-            <Card>
-              <CardHeader>
-                <CardTitle className="flex gap-2">
-                  <StackIcon /> Inspect your database
-                </CardTitle>
-              </CardHeader>
-              <CardContent>
-                The{" "}
-                <Link
-                  href="https://dashboard.convex.dev/"
-                  className="underline underline-offset-4 hover:no-underline"
-                  target="_blank"
-                >
-                  Convex dashboard
-                </Link>{" "}
-                is already open in another window.
-              </CardContent>
-            </Card>
-            <Card>
-              <CardHeader>
-                <CardTitle className="flex gap-2">
-                  <CodeIcon />
-                  Change the backend
-                </CardTitle>
-              </CardHeader>
-              <CardContent>
-                Edit <Code>convex/messages.ts</Code> to change the backend
-                functionality.
-              </CardContent>
-            </Card>
-            <Card>
-              <CardHeader>
-                <CardTitle className="flex gap-2">
-                  <MagicWandIcon />
-                  Change the frontend
-                </CardTitle>
-              </CardHeader>
-              <CardContent>
-                Edit <Code>app/page.tsx</Code> to change your frontend.
-              </CardContent>
-            </Card>
-          </div>
-        </div> */}
       </div>
-      {/* <div className="px-20 pb-20">
-        <div className="container">
-          <h2 className="mb-6 text-center text-2xl font-bold">
-            Helpful resources
-          </h2>
-          <div className="grid gap-6 md:grid-cols-4">
-            <Resource title="Convex Docs" href="https://docs.convex.dev/home">
-              Read comprehensive documentation for all Convex features.
-            </Resource>
-            <Resource title="Stack articles" href="https://stack.convex.dev/">
-              Learn about best practices, use cases, and more from a growing
-              collection of articles, videos, and walkthroughs.
-            </Resource>
-            <Resource title="Discord" href="https://www.convex.dev/community">
-              Join our developer community to ask questions, trade tips &
-              tricks, and show off your projects.
-            </Resource>
-            <Resource title="Search them all" href="https://search.convex.dev/">
-              Get unblocked quickly by searching across the docs, Stack, and
-              Discord chats.
-            </Resource>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 };
